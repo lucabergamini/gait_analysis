fa317e396be3f81c40233420a5835dc96829f3e1
berga2040 <lucabergamini24@gmail.com>
2017-04-29 22:41:05 +0200

    Add layer for LSTM output slice

---
+++ learn/layers.py
@@ -0,0 +1,22 @@
+from keras.layers import Layer
+import numpy
+import theano.tensor as T
+
+
+class MyLayer(Layer):
+
+    def __init__(self,size, **kwargs):
+        super(MyLayer, self).__init__(**kwargs)
+        self.size = size
+
+
+    def build(self, input_shape):
+        # non ho pesi!.
+        super(MyLayer, self).build(input_shape)  # Be sure to call this somewhere!
+
+    def call(self, x,**kwargs):
+        return x[:,-self.size:]
+
+    def compute_output_shape(self, input_shape):
+        input_shape[1] = self.size
+        return input_shape
